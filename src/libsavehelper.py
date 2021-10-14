ed653c7cbc60c3b0067ef16e0157884baeb96762
EarthlySkies <92538843+EarthlySkies@users.noreply.github.com>
2021-10-14 17:37:15 +0000

    Create libsavehelper.py
    Created the preliminary library file for the main executable

---
+++ src/libsavehelper.py
@@ -0,0 +1,7 @@
+# A part of the "kura5-savehelper" project
+# This is the main library file "savehelper.py" will be utlizing
+# All functions used inside 'main' are to be defined here
+
+import os
+import platform
+import sys
