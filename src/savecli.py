4c4b8b79c7d32e32651d905e85d48d4855a5408e
EarthlySkies <92538843+EarthlySkies@users.noreply.github.com>
2021-10-16 07:39:44 +0000

    Update savecli.py
    Cleaned up the language and added an operation for deleting all backed up saves

--- src/savecli.py
+++ src/savecli.py
@@ -4,10 +4,11 @@ import libsavehelper as lib
 
 # Preliminary CLI
 # Kura5 Savehelper v. (__version__)
-# Kura5 version ( int / "Unknown" )
+# Kura5 version: ( int / "Unknown" )
 # The following operations are available:
 # 1 - Create a backup copy of a currently active save
 # 2 - Load a backup save into a save slot
-# 3 - Open backup directory in file explorer
+# 3 - Open the backup directory in file explorer
+# 4 - Delete all backed up saves from the system
 # 0 - Exit
 # Please input your selected operation: 
