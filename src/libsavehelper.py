b5185a5c18bef53fe6782d30b74d368d97b567b4
EarthlySkies <92538843+EarthlySkies@users.noreply.github.com>
2021-10-17 16:27:11 +0000

    Update libsavehelper.py
    Fixed a missing path directory in fetch_backupdir_abspath

--- src/libsavehelper.py
+++ src/libsavehelper.py
@@ -32,8 +32,8 @@ def does_kura5devsdir_exist(env: dict):
 # TODO: add function description
 def fetch_backupdir_abspath(env: dict):
     if env["Platform"] == "Windows":
-        backupdirAbspath = os.path.expandvars("%userprofile%/LocalLow/Kura5 Devs"
-                                              "/Kura5/Savehelper")
+        backupdirAbspath = os.path.expandvars("%userprofile%/AppData/LocalLow/"
+                                              "Kura5 Devs/Kura5/Savehelper")
         return backupdirAbspath
     if env["Platform"] == "Linux" or "Darwin":
         backupdirAbspath = os.path.expanduser("~/.config/unity3d/Kura5 Devs"
